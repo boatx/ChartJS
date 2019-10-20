8ca724c1b6ba76833db00a58a1bda678f8a297d1
boatx <1446337+boatx@users.noreply.github.com>
2019-10-20 11:03:27 +0200

    Add webpack config

--- src/index.js
+++ src/index.js
@@ -190,3 +190,21 @@ var chartJS = (function () {
         }
     };
 }());
+
+var setup = (function() {
+    var elements = {
+        dogs: 0.3,
+        cats: 0.6,
+        dinosaurs: 0.1
+    };
+
+    var colors = {
+        dogs: 'green',
+        cats: 'blue',
+        dinosaurs: 'red'
+    };
+
+    var canvas = document.getElementById('can');
+    var chart = chartJS.PieChart(elements, colors, canvas);
+    chart.draw();
+}());
